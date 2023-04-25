80d5b0f5eafe31d2e9f14b11ee421d6f6f7e796e
ShivamJNU <119946005+ShivamJNU@users.noreply.github.com>
2023-04-26 00:18:22 +0530

    Add files via upload

---
+++ Test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <vector>
+
+#define ll long long
+using namespace std;
+
+int main()
+{
+
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		
+	}
+	return 0;
+}
