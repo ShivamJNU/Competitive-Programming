f547239e6c46bad586c1603aac05be846c68f76b
ShivamJNU <119946005+ShivamJNU@users.noreply.github.com>
2024-02-14 23:48:06 +0530

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -10,6 +10,14 @@ using namespace std;
 
 #define MOD 1000000007
 
+#define REP(j, a, b) for (int j = a; j < b; j++)
+#define print(x) cout << x << endl
+#define yep cout << "YES" << endl
+#define nope cout << "NO" << endl
+
+#define int long long
+#define sorted(x) sort(x.begin(), x.end())
+
 void solve()
 {
     int n;
@@ -24,7 +32,7 @@ void solve()
     return;
 }
 
-int main()
+signed main()
 {
     ios_base::sync_with_stdio(false); 
     cin.tie(NULL); cout.tie(NULL);
@@ -38,3 +46,38 @@ int main()
     }
     return 0;
 }
+
+/* --> There is an inbuilt GCD function in STL: __gcd(a, b)
+   --> LCM (a, b) = (a*b)/GCD (a, b)
+  
+   --> To check the ith bit in a number: (N>>i) & 1 
+  (Here the index starts from 0 and goes till 30 i.e. total 31 bits can be checked as the MSB is for sign)
+  
+   --> To calculate the powers of 2 use LSO: (1<<i)
+   --> To check if a number is a power of 2: N & (N-1) == 0
+  
+   --> To check the ith bit in a number: (N>>i) & 1 
+
+
+*/
+
+/*
+   --> THE FORMAT OF BINARY SEARCH ON ANSWER:
+   
+   int left, right;
+
+    while (right - left > 1)
+    {
+        int mid = (right + left) / 2;
+
+        if (check(mid))
+        {
+            right = mid;
+        }
+        else
+        {
+            left = mid;
+        }
+    }
+
+*/
