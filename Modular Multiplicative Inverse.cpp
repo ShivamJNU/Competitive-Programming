32c1f992d4a8cbe79b1f34035f23c23fb1bfc3fa
ShivamJNU <119946005+ShivamJNU@users.noreply.github.com>
2024-09-02 02:36:21 +0530

    Create Modular Multiplicative Inverse.cpp

---
+++ Modular Multiplicative Inverse.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int mod = 1e9 + 7;
+
+int bin_exp(int a, int b) {
+    if (b == 0) {
+        return 1;
+    }
+    if (b % 2) {
+        return bin_exp(a, b - 1) * 1LL * a % mod;
+    }
+    int res = bin_exp(a, b / 2);
+    return res * 1LL * res % mod;
+}
+
+int inv(int a) {
+    return bin_exp(a, mod - 2);
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    int a, b;
+    cin >> a >> b;
+    cout << a * 1LL * inv(b) % mod << '\n';
+}
