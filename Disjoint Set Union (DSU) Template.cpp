d69e4d4a94d8e46905f311dead2dc116466941af
ShivamJNU <119946005+ShivamJNU@users.noreply.github.com>
2025-01-27 01:57:47 +0530

    Create Disjoint Set Union (DSU) Template.cpp

---
+++ Disjoint Set Union (DSU) Template.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Time Complexity of DSU: Amortized time complexity: Value increases very slowly
+
+const int N = 1e5 + 10;
+
+int parent[N];
+int size[N];
+
+void make(int v)
+{
+    parent[v] = v;
+    size[v] = 1;
+}
+
+int find(int v)
+{
+    if (parent[v] == v)
+    {
+        return v;
+    }
+
+    // Path Compression
+    return parent[v] = find(parent[v]);
+}
+
+void Union(int a, int b)
+{
+    a = find(a);
+    b = find(b);
+
+    if (a != b)
+    {
+        // Tree of a should have greater size than Tree of b for optimisation
+        if (size[a] < size[b])
+        {
+            swap(a, b);
+        }
+        parent[b] = a;
+        size[a] += b;
+        return;
+    }
+}
+
+void solve(int t, int NN)
+{
+    int n, k;
+    cin >> n >> k;
+
+    for (int i = 0; i < n; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+
+        Union(u, v);
+    }
+
+    int connectedComponentsCount = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (find(i) == i)
+        {
+            connectedComponentsCount++;
+        }
+    }
+
+    cout << connectedComponentsCount << endl;
+
+    return;
+}
